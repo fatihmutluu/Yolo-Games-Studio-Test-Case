0f4b4c99e2852a31399cdfeb45d4056bba30515e
Fatih Mutlu <fatih.2000.mutlu@gmail.com>
2024-05-12 01:04:25 +0300

    alpha

---
+++ Assets/Scripts/AlignImages.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+
+public static class BoundLimits
+{
+    public static float upLeft;
+    public static float upRight;
+    public static float upTop;
+    public static float upBottom;
+    public static float downLeft;
+    public static float downRight;
+    public static float downTop;
+    public static float downBottom;
+}
+
+public class AlignImages : MonoBehaviour
+{
+    private GameObject upObject;
+    private GameObject downObject;
+
+    private void Awake()
+    {
+        upObject = GameObject.Find("Up Background");
+        downObject = GameObject.Find("Down Background");
+        SetupBoundLimits();
+    }
+
+    private void Update()
+    {
+        Align();
+    }
+
+    private void Align()
+    {
+        var upSpriteRender = upObject.GetComponent<BoxCollider2D>();
+        var upBounds = upSpriteRender.bounds;
+        var upTopRight = upBounds.max;
+        var upBottomLeft = upBounds.min;
+
+        Vector3 upNewPosition = upObject.transform.position;
+        Vector3 downNewPosition = downObject.transform.position;
+
+        // Adjust horizontal positions
+        if (upBottomLeft.x > BoundLimits.upLeft)
+        {
+            upNewPosition.x = BoundLimits.upLeft + upBounds.size.x / 2;
+            downNewPosition.x = BoundLimits.downLeft + upBounds.size.x / 2;
+        }
+        if (upTopRight.x < BoundLimits.upRight)
+        {
+            upNewPosition.x = BoundLimits.upRight - upBounds.size.x / 2;
+            downNewPosition.x = BoundLimits.downRight - upBounds.size.x / 2;
+        }
+
+        // Adjust vertical positions
+        if (upTopRight.y < BoundLimits.upTop)
+        {
+            upNewPosition.y = BoundLimits.upTop - upBounds.size.y / 2;
+            downNewPosition.y = BoundLimits.downTop - upBounds.size.y / 2;
+        }
+        if (upBottomLeft.y > BoundLimits.upBottom)
+        {
+            upNewPosition.y = BoundLimits.upBottom + upBounds.size.y / 2;
+            downNewPosition.y = BoundLimits.downBottom + upBounds.size.y / 2;
+        }
+
+        // Set the new positions
+        upObject.transform.position = upNewPosition;
+        downObject.transform.position = downNewPosition;
+    }
+
+    private void SetupBoundLimits()
+    {
+        var upSpriteRender = upObject.GetComponent<BoxCollider2D>();
+        var upBounds = upSpriteRender.bounds;
+        var upTopRight = upBounds.max;
+        var upBottomLeft = upBounds.min;
+
+        var downSpriteRender = downObject.GetComponent<BoxCollider2D>();
+        var downBounds = downSpriteRender.bounds;
+        var downTopRight = downBounds.max;
+        var downBottomLeft = downBounds.min;
+
+        BoundLimits.upLeft = upBottomLeft.x;
+        BoundLimits.upRight = upTopRight.x;
+        BoundLimits.upTop = upTopRight.y;
+        BoundLimits.upBottom = upBottomLeft.y;
+        BoundLimits.downLeft = downBottomLeft.x;
+        BoundLimits.downRight = downTopRight.x;
+        BoundLimits.downTop = downTopRight.y;
+        BoundLimits.downBottom = downBottomLeft.y;
+    }
+}
--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -1,36 +1,18 @@
 using System.Collections.Generic;
 using System.Linq;
-using Unity.Collections;
 using UnityEngine;
-using UnityEngine.UIElements;
-
-public class BoundLimits
-{
-    public float upLeft;
-    public float upRight;
-    public float upTop;
-    public float upBottom;
-    public float downLeft;
-    public float downRight;
-    public float downTop;
-    public float downBottom;
-}
 
 public class GameControl : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject leftCounter;
     public List<GameObject> differences;
 
     private ClickHandler clickHandler;
 
-    [SerializeField]
-    private GameObject leftCounter;
-
-    private bool isDragging = false;
     private Vector3 initialMousePosition;
 
     private EndGameController endGameController;
-
-    private BoundLimits boundLimits;
 
     private void Awake()
     {
@@ -41,74 +23,47 @@ public class GameControl : MonoBehaviour
         {
             Debug.Log(difference.transform.position);
         }
-
-        var upSpriteRender = GameObject.Find("Up Background").GetComponent<BoxCollider2D>();
-        var upBounds = upSpriteRender.bounds;
-        var upTopRight = upBounds.max;
-        var upBottomLeft = upBounds.min;
-
-        var downSpriteRender = GameObject.Find("Down Background").GetComponent<BoxCollider2D>();
-        var downBounds = downSpriteRender.bounds;
-        var downTopRight = downBounds.max;
-        var downBottomLeft = downBounds.min;
-
-        boundLimits = new BoundLimits
-        {
-            upLeft = upBottomLeft.x,
-            upRight = upTopRight.x,
-            upTop = upTopRight.y,
-            upBottom = upBottomLeft.y,
-            downLeft = downBottomLeft.x,
-            downRight = downTopRight.x,
-            downTop = downTopRight.y,
-            downBottom = downBottomLeft.y
-        };
     }
 
     private void Update()
     {
         Debug.Log("Game is running");
+
         if (CheckWin())
-        {
-            Debug.Log("You Win!");
             endGameController.GameWin();
-        }
 
-        // Check for mouse input only if not dragging
-        if (!isDragging)
+        if (Input.GetMouseButtonDown(0))
         {
-            if (Input.GetMouseButtonDown(0))
-            {
-                initialMousePosition = Input.mousePosition; // Store initial mouse position
-            }
-            else if (Input.GetMouseButtonUp(0))
-            {
-                // Calculate the distance moved by the mouse
-                float distance = Vector3.Distance(initialMousePosition, Input.mousePosition);
+            initialMousePosition = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            // Calculate the distance moved by the mouse
+            float distance = Vector3.Distance(initialMousePosition, Input.mousePosition);
 
-                // If the distance is small, treat it as a click, otherwise consider it dragging
-                if (distance < 10f) // Adjust this threshold as needed
+            // If the distance is small, treat it as a click, otherwise consider it dragging
+            if (distance < 10f)
+            {
+                Vector3 mousePosition = Input.mousePosition;
+                mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+                mousePosition.z = 0;
+
+                Debug.Log(mousePosition);
+
+                if (
+                    mousePosition.x < BoundLimits.upLeft
+                    || mousePosition.x > BoundLimits.upRight
+                    || mousePosition.y > BoundLimits.upTop
+                    || mousePosition.y < BoundLimits.downBottom
+                )
                 {
-                    Vector3 mousePosition = Input.mousePosition;
-                    Debug.Log(mousePosition);
-
-                    if (
-                        mousePosition.x < 52.01f
-                        || mousePosition.x > 1027.99f
-                        || mousePosition.y > 1641.15f
-                        || mousePosition.y < 322.42f
-                    )
-                    {
-                        Debug.Log("Out of bounds");
-                        return;
-                    }
-
-                    CheckIfHitDifference(mousePosition);
+                    Debug.Log("Out of bounds");
+                    return;
                 }
+
+                CheckIfHitDifference(mousePosition);
             }
         }
-
-        AlignImages();
     }
 
     private bool CheckWin()
@@ -119,12 +74,6 @@ public class GameControl : MonoBehaviour
     private void CheckIfHitDifference(Vector3 mousePosition)
     {
         Debug.Log("Checking Hit");
-
-        // Convert to world coordinates
-        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        mousePosition.z = 0;
-
-        Debug.Log("Mouse position: " + mousePosition);
 
         // ! look for is mousePosition in hitbox of any difference
         foreach (GameObject difference in differences)
@@ -157,80 +106,5 @@ public class GameControl : MonoBehaviour
         // ! handling miss if no difference was hit
         Debug.Log("Miss!");
         clickHandler.Miss(mousePosition);
-    }
-
-    private void AlignImages()
-    {
-        var upObject = GameObject.Find("Up Background");
-        var upSpriteRender = upObject.GetComponent<BoxCollider2D>();
-        var upBounds = upSpriteRender.bounds;
-        var upTopRight = upBounds.max;
-        var upBottomLeft = upBounds.min;
-
-        var downObject = GameObject.Find("Down Background");
-        var downSpriteRender = downObject.GetComponent<BoxCollider2D>();
-        var downBounds = downSpriteRender.bounds;
-        var downTopRight = downBounds.max;
-        var downBottomLeft = downBounds.min;
-
-        if (upBottomLeft.x > boundLimits.upLeft)
-        {
-            upObject.transform.position = new Vector3(
-                boundLimits.upLeft + upBounds.size.x / 2,
-                upObject.transform.position.y,
-                upObject.transform.position.z
-            );
-
-            downObject.transform.position = new Vector3(
-                boundLimits.downLeft + downBounds.size.x / 2,
-                downObject.transform.position.y,
-                downObject.transform.position.z
-            );
-        }
-
-        if (upTopRight.x < boundLimits.upRight)
-        {
-            upObject.transform.position = new Vector3(
-                boundLimits.upRight - upBounds.size.x / 2,
-                upObject.transform.position.y,
-                upObject.transform.position.z
-            );
-
-            downObject.transform.position = new Vector3(
-                boundLimits.downRight - downBounds.size.x / 2,
-                downObject.transform.position.y,
-                downObject.transform.position.z
-            );
-        }
-
-        if (upTopRight.y < boundLimits.upTop)
-        {
-            upObject.transform.position = new Vector3(
-                upObject.transform.position.x,
-                boundLimits.upTop - upBounds.size.y / 2,
-                upObject.transform.position.z
-            );
-
-            downObject.transform.position = new Vector3(
-                downObject.transform.position.x,
-                boundLimits.downTop - downBounds.size.y / 2,
-                downObject.transform.position.z
-            );
-        }
-
-        if (upBottomLeft.y > boundLimits.upBottom)
-        {
-            upObject.transform.position = new Vector3(
-                upObject.transform.position.x,
-                boundLimits.upBottom + upBounds.size.y / 2,
-                upObject.transform.position.z
-            );
-
-            downObject.transform.position = new Vector3(
-                downObject.transform.position.x,
-                boundLimits.downBottom + downBounds.size.y / 2,
-                downObject.transform.position.z
-            );
-        }
     }
 }
